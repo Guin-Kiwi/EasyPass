8b5530a5adb60a93f4b5323253a29cba12eed20e
Polina Yemelianenkova <145330202+polina-yemelianenkova@users.noreply.github.com>
2025-11-02 12:47:27 +0000

    password generator added the code

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,13 @@
-password generate
+import random
+import string
+
+def password_generator(length):
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    password = ""
+    for i in range(length):
+        password += random.choice(characters)
+
+length = int(input("Enter password length: "))
+print("Generated password:", password_generator(length))
